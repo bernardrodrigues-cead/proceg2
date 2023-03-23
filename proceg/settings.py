37a3a0c6722da46e05d865c3a8caa7b43f849e27
bernardrodrigues-cead <bernard.rodrigues@uab.ufjf.br>
2023-03-23 17:51:36 -0300

    add scripts folder

--- proceg/settings.py
+++ proceg/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import json
 from pathlib import Path
+
+with open('scripts/config.json', 'r') as f:
+    CONFIG = json.load(f)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9^c42(2@i6rf+t343ukkby=hgfcdxc2rw99#+tc@4!ikzska@qv'
+SECRET_KEY = CONFIG['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
